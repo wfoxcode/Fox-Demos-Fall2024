87c9169d1ec53f9de72380ae39572c0e11718e07
Webster Fox <w.fox@rcsd.ca>
2024-09-16 08:53:01 -0600

    fridays edits

---
+++ 1-Lessons and Demos/Lesson4Demo/src/App.java
@@ -0,0 +1,12 @@
+// Project: Lesson 4 Demo
+// Name: Mr. Fox
+// Date: 13 Sept 2024
+// Description: Going over comments and special characters
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, Wor\\ld!");
+        System.out.println("How is your day going");
+        System.out.println("It\'s \"cloudy\" with a chance of meatballs today!");
+    }
+}
