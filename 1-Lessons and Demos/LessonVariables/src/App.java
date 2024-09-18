ee93acec23456354f4bd27e60f703f381470e914
Webster Fox <w.fox@rcsd.ca>
2024-09-18 08:52:37 -0600

    tuesday demos

---
+++ 1-Lessons and Demos/LessonVariables/src/App.java
@@ -0,0 +1,17 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // System.out.println(2 / 3);
+        // System.out.println(2.0 / 3);
+        // System.out.println( (double) 2 / 3);
+
+        // int myNum = 2147483647;
+        // System.out.println(myNum);
+        // myNum = myNum + 1;
+        // System.out.println(myNum);
+        // System.out.println(Integer.MAX_VALUE);
+
+        int num = (int) Math.random() * 10;
+        System.out.println(num);
+
+    }
+}
